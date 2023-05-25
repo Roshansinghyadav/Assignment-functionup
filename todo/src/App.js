4acfc7eba198f43892a8d9a487597f824c1b950f
Roshan singh yadav <callmersy8031@gmail.com>
2023-05-25 11:24:30 +0530

    this is redux code

---
+++ todo/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Counter from './Atom/Task Counters/taskcounter';
+import Display from './Atom/Task Display/taskdisplay';
+import Input from './Atom/Task Input/taskinput';
+
+function App() {
+  
+  return (
+    <>
+    <div className='App'> 
+
+<Counter/>
+<Display/>
+<Input/>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
+
